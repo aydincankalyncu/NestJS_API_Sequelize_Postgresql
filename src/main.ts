62d598d28ec258f8178e6f6b0fc67c4be13f7a45
aydincan.kalyoncu <aydincan.kalyoncu@ARV-93>
2022-08-12 15:20:46 +0300

    Swagger added.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Authenticate and make crud operations for posts.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
   //Global prefix
   app.setGlobalPrefix('api/v1');
   //app.useGlobalPipes(new ValidateInputPipe());
